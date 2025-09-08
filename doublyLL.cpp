d3cc0ae2a6e9bfebe20de49cabc741949959f4ac
ActuallyMadhav <madhavra.2004@gmail.com>
2025-09-09 00:14:09 +0800

    doubly linked list

--- doublyLL.cpp
+++ doublyLL.cpp
@@ -6,3 +6,53 @@ struct Node{
     Node* next;
 };
 
+void printForward(Node* head){
+    Node* cur = head;
+    while(cur){
+        std::cout << cur->val << " -> ";
+        cur = cur->next;
+    }
+    std::cout << "NULL" << '\n';
+}
+
+void printReverse(Node* head){
+    Node* cur = head;
+    while(cur->next){cur = cur->next;}
+
+    // reverse traversal
+    while(cur){
+        std::cout << cur->val << " -> ";
+        cur = cur->prev;
+    }
+    std::cout << "Reached Head";
+}
+
+void insertAtHead(Node*& head, int x){
+    Node* newNode = new Node;
+    newNode->val = x;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+
+    if(head == nullptr){
+        head = newNode;
+        return;
+    }
+    else{
+        head->prev = newNode;
+        newNode->next = head;
+        head = newNode; 
+    }
+}
+
+int main(){
+    
+    Node* head = nullptr;
+    printForward(head);
+    insertAtHead(head, 1);
+    printForward(head);
+    insertAtHead(head, 2);
+    printForward(head);
+    printReverse(head);
+
+    return 0;
+}
