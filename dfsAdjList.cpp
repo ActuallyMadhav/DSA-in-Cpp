ce6d9b227b16e49b27982ce19605b1b2547b9abd
ActuallyMadhav <madhavra.2004@gmail.com>
2025-10-18 19:02:46 +0800

    included test input for both DFS versions

--- dfsAdjList.cpp
+++ dfsAdjList.cpp
@@ -11,3 +11,19 @@ void dfs(std::vector<std::vector<int>>& AdjList, std::vector<bool>& visited, int
         }
     }
 }
+
+int main(){
+
+    std::vector<std::vector<int>> adjList = {
+        {1, 2},    // Node 0
+        {0, 3},    // Node 1
+        {0, 4},    // Node 2
+        {1},       // Node 3
+        {2}        // Node 4
+    };
+
+    std::vector<bool> visited(adjList.size(), false);
+
+    dfs(adjList, visited, 0);
+    return 0;
+}
--- dfsAdjMat.cpp
+++ dfsAdjMat.cpp
@@ -13,3 +13,20 @@ void dfs(std::vector<std::vector<int>>& A, std::vector<bool>& visited, int s){
         }
     }
 }
+
+int main() {
+    // Adjacency matrix representation of the graph
+    std::vector<std::vector<int>> adjMatrix = {
+        {0, 1, 1, 0, 0}, // Node 0
+        {1, 0, 0, 1, 0}, // Node 1
+        {1, 0, 0, 0, 1}, // Node 2
+        {0, 1, 0, 0, 0}, // Node 3
+        {0, 0, 1, 0, 0}  // Node 4
+    };
+
+    std::vector<bool> visited(adjMatrix.size(), 0);
+
+    // Perform DFS starting from node 0
+    dfs(adjMatrix, visited, 0);
+    return 0;
+}
