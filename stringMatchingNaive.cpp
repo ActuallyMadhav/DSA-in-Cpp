21d50f5cd1a312befa65cca62efebc511abf00d3
ActuallyMadhav <madhavra.2004@gmail.com>
2025-11-19 11:03:39 +0800

    brute force from Michael T.G. textbook

--- stringMatchingNaive.cpp
+++ stringMatchingNaive.cpp
@@ -2,23 +2,40 @@
 #include <algorithm>
 #include <vector>
 
-int stringMatch(const std::string& inputStr, const std::string& pattern){
-    int n = inputStr.size();
-    int m = pattern.size();
+// int stringMatch(const std::string& inputStr, const std::string& pattern){
+//     int n = inputStr.size();
+//     int m = pattern.size();
 
-    std::vector<int> indices;
+//     std::vector<int> indices;
 
-    // try every possible starting index
-    for(int i = 0; i <= n - m; i++){
-        int j = 0;
+//     // try every possible starting index
+//     for(int i = 0; i <= n - m; i++){
+//         int j = 0;
         
-        // compare pattern with substrs of inputStr
-        while(j < m && inputStr[i+j] == pattern[j]){
+//         // compare pattern with substrs of inputStr
+//         while(j < m && inputStr[i+j] == pattern[j]){
+//             j++;
+//         }
+//         if(j == m){
+//             return i; // match found at index i
+//         }
+//     }
+//     return -1; // match not found
+// }
+
+// alternatively:
+
+int bruteForceMatch(std::string pattern, std::string text){
+    int textSize = text.size();
+    int patternSize = pattern.size();
+
+    for(int i = 0; i < textSize - patternSize; i++){
+        int j = 0;
+
+        while(j < patternSize && text[i + j] == pattern[j]){
             j++;
         }
-        if(j == m){
-            return i; // match found at index i
-        }
+        if(j == patternSize) return i;
+        return -1;  // no match found
     }
-    return -1; // match not found
 }
